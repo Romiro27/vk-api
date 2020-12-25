31a22474fae372a550c7b9b4a2114280870e0477
Ivan Rekunov <vanno6521@gmail.com>
2020-12-26 02:21:17 +0400

    Longpoll update
    RU: Обновление лонгполла, теперь он польностью рабочий и пригоден для использования

--- include/longpoll.h
+++ include/longpoll.h
@@ -7,6 +7,11 @@
 #pragma once
 
 #include <api.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <json/json.hpp>
+#include <http/http.h>
 
 namespace vk
 {
@@ -15,17 +20,37 @@ namespace vk
         public:
             Longpoll(Api &vkapi, int gid);
 
+            void get_settings();
+            void set_settings(nlohmann::ordered_json settings);
+
+            template<typename T>
+            void handler(T user_query)
+            {
+                std::lock_guard<std::mutex> lock(this->mute);
+                
+                if(this->is_update == 1)
+                {
+                    user_query();
+                    this->is_update = 0;
+                }
+            }
+
+            nlohmann::ordered_json settings;
+            nlohmann::ordered_json event = { {"event_id", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"} };
+
         private:
+            bool is_update;
+
             int group_id;
             Api api;
+            Http HTTP;
 
-            struct _session
-            {
-                std::string key;
-                std::string server;
-                std::string ts;
-            } session;
+            nlohmann::ordered_json session;
 
-            void create_longpoll(int gid);
+            void get_session(int gid);
+
+            std::thread lp;
+            std::mutex mute;
+            void longpoll();
     };
 }
--- src/longpoll.cpp
+++ src/longpoll.cpp
@@ -5,8 +5,12 @@
  */
 
 #include <api.h>
+#include <string>
+#include <thread>
+#include <mutex>
 #include <longpoll.h>
 #include <json/json.hpp>
+#include <http/http.h>
 
 namespace vk
 {
@@ -15,10 +19,13 @@ namespace vk
         this->api      = vkapi;
         this->group_id = gid;
 
-        this->create_longpoll(gid);
+        this->get_session(gid);
+
+        this->lp = std::thread(&Longpoll::longpoll, this);
+        this->lp.detach();
     }
 
-    void Longpoll::create_longpoll(int gid)
+    void Longpoll::get_session(int gid)
     {
         nlohmann::ordered_json resp = this->api.method("groups.getLongPollServer",
             {
@@ -27,15 +34,53 @@ namespace vk
 
         if(!(resp.find("error") != resp.end()))
         {
-            this->session.key = resp.at("response").at("key");
-            this->session.server = resp.at("response").at("server");
-            this->session.ts = resp.at("response").at("ts");
+            this->session = resp.at("response");
         }
         else
         {
             std::string err = "Longpoll create fail: ";
             err += resp.at("error").at("error_msg");
             throw std::runtime_error(err);
+        }
+    }
+
+    void Longpoll::get_settings()
+    {
+        this->settings = this->api.method("groups.getLongPollSettings",
+            {
+                { "group_id:", this->group_id }
+            });
+    }
+
+    void Longpoll::set_settings(nlohmann::ordered_json settings)
+    {
+        this->api.method("groups.setLongPollSettings", settings);
+
+        this->get_settings();
+    }
+
+    void Longpoll::longpoll()
+    {
+        for(;;)
+        {
+            nlohmann::ordered_json resp = this->HTTP.GET(
+                this->session.at("server").get<std::string>() +
+                "?act=a_check&key=" +
+                this->session.at("key").get<std::string>() +
+                "&ts=" +
+                this->session.at("ts").get<std::string>() +
+                "&wait=25"
+            );
+
+            std::lock_guard<std::mutex> lock(this->mute);
+
+            if(this->event.at("event_id") != resp.at("updates")[0]["event_id"])
+            {
+                this->is_update = true;
+
+                this->session.at("ts") = resp.at("ts");
+                this->event = resp.at("updates")[0];
+            }
         }
     }
 }
